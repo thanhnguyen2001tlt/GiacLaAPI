081fa53b1b0d2c4860484a0ff8519b986599a94f
Tdev2023VFA <Tdev2023VFA@gmail.com>
2023-06-15 10:43:34 +0700

    update

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const app = express();
 app.use(express.json());
 const db = require("./config/key").mogoURI;
 mongoose.set('strictQuery', false);
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, }).then(() => console.log("MongoDB Connected....")).catch((err) => console.log(err));
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("MongoDB Connected....")).catch((err) => console.log(err));
+
+// Thiết lập tiêu đề CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Cho phép truy cập từ bất kỳ nguồn nào
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use("/api/", require("./router/customerRouter"));
 app.use("/api/", require("./router/employeeRouter"));
@@ -13,7 +23,5 @@ app.use("/api/", require("./router/serviceRouter"));
 app.use("/api/", require("./router/orderRouter"));
 app.use("/api/", require("./router/orderDetailRouter"));
 
-
 const PORT = process.env.PORT || 8080;
-app.listen(PORT) 
-
+app.listen(PORT);
