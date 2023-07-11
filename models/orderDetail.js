3d1b4e5283b8e7dd3c7a78d3054bf302437bb262
Tdev2023VFA <Tdev2023VFA@gmail.com>
2023-07-12 03:16:19 +0700

    update

--- models/orderDetail.js
+++ models/orderDetail.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const OrderDetailSchema = new mongoose.Schema({
   order: { type: mongoose.Schema.Types.ObjectId, ref: 'Order' },
-  service: { type: mongoose.Schema.Types.ObjectId, ref: 'Service' },
+  service: String,
   servicePrice: Number,
   quantity: Number,
   subtotal: Number
